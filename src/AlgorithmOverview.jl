0e7a8c547d8734594d52bda8aa25fe65cc67dc6d
RearrangedLetters <RobinLink@ProtonMail.com>
2022-11-17 23:33:13 +0100

    Add overview of implemented algorithms.

---
+++ src/AlgorithmOverview.jl
@@ -0,0 +1,25 @@
+""" Overview over the implemented algorithms and open things.
+Symbols:
+    • (I) : implemented
+    • (P) : partially implemented
+    • (N) : not implemented
+    • (O) : optimization known
+    • (T) : tested
+    • (A) : indirectly availble, i.e. with slight modification/extension
+
+Chapter 01 & 02 - Orbits and Stabilizers
+    • Orbit (I)
+    • Transversal
+        ◀ Orbit-Transversal (I)
+        ◀ Orbit-Factored-Transversal (I)
+        ◀ Factored-Transversal-Reconstruction (I)
+    • Normal Closure (P)
+    • Commutator Subgroup (N)
+    • Pseudorandom Elements aka Product Replacement (I)
+
+Chapter 03 - Stabilizer Chains
+    • 
+
+Chapter 04 - 
+
+"""
